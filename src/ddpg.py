a3b302bd5091c8fb9e83b806c8183aee345f2a6f
Shixin Li <leept416@gmail.com>
2018-02-21 16:46:40 -0800

    modifiying ddpg

--- src/ddpg.py
+++ src/ddpg.py
@@ -6,7 +6,24 @@ import numpy as np
 import tensorflow as tf
 import time
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# tensorflow log dir
+TF_LOG_DIR = dir_path + '/../log'
+
+# tensorflow output dir
+TF_OUT_DIR = dir_path + '/../output'
+
 
 class DDPG:
 
     def __init__(self):
+
+        if not tf.gfile.Exists(TF_LOG_DIR):
+            tf.gfile.MakeDirs(TF_LOG_DIR)
+        if not tf.gfile.Exists(TF_OUT_DIR):
+            tf.gfile.MakeDirs(TF_OUT_DIR)
+
+        self.session = tf.session()
+
+        self.memory
