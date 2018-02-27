56bd80f5c1f42c7711b2300b1251cfbd02082a3d
Shixin Li <leept416@gmail.com>
2018-02-27 14:56:09 -0800

    finish send robot to correct starting pos

--- src/ros_handler.py
+++ src/ros_handler.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pickle
+import cPickle as pickle
 import rospy
 import numpy as np
+import random
+import tf
 from gazebo_msgs.msg import ModelState
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Point
@@ -40,7 +42,7 @@ class RosHandler:
         self._sub_robot_pos = rospy.Subscriber(
             "/ground_truth/state", Odometry, self._input_callback_robot_pos)
         self._sub_new_start = rospy.Subscriber(
-            "/reach_start", Point, self._gazebo_callback_new_start)
+            "/new_start", Point, self._gazebo_callback_new_start)
         self._sub_end_traj = rospy.Subscriber(
             "/end_traj", Bool, self._gazebo_callback_end_traj)
 
@@ -50,8 +52,10 @@ class RosHandler:
         self._end_of_episode = Bool()
         self._end_of_episode.data = True
 
-        # cost_map = rospy.get_param('costmap')
-        # self._cost_map = pickle.load(costmap)
+        cost_map = rospy.get_param('costmap')
+        with open(cost_map, 'rb') as pickle_file:
+            self._cost_map = pickle.load(pickle_file)
+        # self._cost_map = pickle.load(cost_map)
 
         rospy.init_node("ros_handler", anonymous=True)
 
@@ -75,22 +79,69 @@ class RosHandler:
         self._robot_pos[1] = data.pose.pose.position.y
 
     def _gazebo_callback_new_start(self, data):
-        msg = ModelState()
-        msg.pose.position.x = self._person_pos[0]
-        msg.pose.position.y = self._person_pos[1]
-        msg.model_name = "freight"
+        # msg = ModelState()
+        # msg.pose.position.x = self._person_pos[0]
+        # msg.pose.position.y = self._person_pos[1]
+        # msg.model_name = "freight"
         # data: Point message
-        x, y = _calculate_start_pos(data)
-        self._pub_robot_pos.publish(msg)
+        # print "ever here?? 86"
+        # print "=============== the target is: ", data
+        # print "=============== the human isL ", self._person_pos
+        self._calculate_start_pos(data, self._person_pos)
+
+    def _gazebo_callback_end_traj(self, data):
+        pass
 
     def _calculate_start_pos(self, target, actor_pos):
         dx = target.x - actor_pos[0]
         dy = target.y - actor_pos[1]
-        radius = random.uniform(0.3, 1)
+        vec = np.array([dx, dy])
+        norm = np.linalg.norm(vec)
+        vec = vec / norm
+        ortho = np.array([vec[1], -vec[0]])
+        success = False
+        result = np.zeros((2, 1))
 
+        while success is False:
+
+            delta_r = random.uniform(0.5, 1)
+            temp = np.array([actor_pos[0]-delta_r*vec[0],
+                             actor_pos[1]-delta_r*vec[1]])
+            noise = random.uniform(-0.2, 0.2)
+            result[0] = temp[0] + noise * ortho[0]
+            result[1] = temp[1] + noise * ortho[1]
+
+            success = self._valid_pos(result)
+
+        msg = ModelState()
+        msg.model_name = "freight"
+        msg.pose.position.x, msg.pose.position.y = result[0], result[1]
+        quaternion = self._yaw_to_quaternion(vec)
+        msg.pose.orientation.x = quaternion[0]
+        msg.pose.orientation.y = quaternion[1]
+        msg.pose.orientation.z = quaternion[2]
+        msg.pose.orientation.w = quaternion[3]
+        self._pub_robot_pos.publish(msg)
+
+    def _valid_pos(self, pos):
+        """
+        check if this place is in safe zone for robot
+        """
+        pos_ = [pos[0], pos[1]]
+        pos_ = np.array(pos_ + [10, 10]) * 100
+        if self._cost_map[int(pos_[0]), int(pos_[1])] == 0:
+            return True
+        else:
+            return False
 
     def _calculate_reward(self):
         pass
+
+    @staticmethod
+    def _yaw_to_quaternion(vec):
+        z = np.arctan2(vec[1], vec[0])
+        quaternion = tf.transformations.quaternion_from_euler(0, 0, z)
+        return quaternion
 
     @property
     def action(self):
